0ceab85049bfa2da79d3af3cc5ecdf55a6f30ba3
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-06 20:58:42 +0530

    Insert data in bulk

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ const connection = mysql.createConnection({
     password:"876787",
 });
 
+let  getRandomUser = () =>  {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+}
+
+
 // INSERTING NEW DATA 
 let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-let users = [
-    ["123b", "123_newuserb", "abc@gmail.comb", "abcb"],
-    ["123c", "123_newuserc", "abc@gmail.comc", "abcc"],
-];
+
+let data = [];
+
+for(let i=1; i <= 100; i++) {
+    data.push(getRandomUser());
+}
 
 
 try {
-    connection.query(q, [users], (err, result) => {
+    connection.query(q, [data], (err, result) => {
         if(err) throw err;
         console.log(result);
     });
@@ -26,17 +38,5 @@ try {
 }
 
 connection.end();
-
-let  getRandomUser = () =>  {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-  };
-}
 
 
